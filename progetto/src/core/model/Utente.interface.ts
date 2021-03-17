0028ec1e218c4a4112ab8b01868d6b9eeb57f039
Louis-Guiniot <louis99679@gmail.com>
2021-03-17 16:16:08 +0100

    creazione prima interface di utente

---
+++ progetto/src/core/model/Utente.interface.ts
@@ -0,0 +1,12 @@
+export interface Utente {
+
+    id?:number,
+    nome:string,
+    congome:string,
+    username:string,
+    password:string,
+    email:string,
+    genere:string,
+    datanascita:string
+
+}
