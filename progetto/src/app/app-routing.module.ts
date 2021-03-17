5b39331701eb890d2608908639f23d6a4ba0fea5
Louis-Guiniot <louis99679@gmail.com>
2021-03-17 23:52:26 +0100

    aggiunte tutte le route per le pagine

--- progetto/src/app/app-routing.module.ts
+++ progetto/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)},
+  {path:'login', loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule)},
+  {path:'profile', loadChildren: () => import('./components/profilo/profilo.module').then(m => m.ProfiloModule)},
+  {path:'tornei', loadChildren: () => import('./components/tornei/tornei.module').then(m => m.TorneiModule)},
+  {path:'miei-tornei', loadChildren: () => import('./components/miei-tornei/miei-tornei.module').then(m => m.MieiTorneiModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
