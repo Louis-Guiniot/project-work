e70abc1c0ad5347970da86f46681820f0de001cc
Louis-Guiniot <louis99679@gmail.com>
2021-03-18 18:24:23 +0100

    index redux miei tornei

---
+++ progetto/src/app/redux/miei-tornei/index.ts
@@ -0,0 +1,16 @@
+import { Params } from "@angular/router";
+import { createSelector } from "@ngrx/store";
+import { AppState } from "..";
+import { TorneoState } from "./miei.tornei.reducers";
+
+export const selectTorneoState = (state: AppState) => state.torneoState;
+
+export const selectTorneo = createSelector(
+    selectTorneoState,
+    (state: TorneoState) => state.arrayTornei
+);
+
+export const getCurrentTorneo = createSelector(
+    selectTorneoState,
+    (state: TorneoState, params: Params) => state.arrayTornei.find(item => item.id === Number(params['id']))
+);
