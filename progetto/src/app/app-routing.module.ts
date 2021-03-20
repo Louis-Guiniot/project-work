25104fa91b1f42ff7154c903753de1b65b939fd2
Louis-Guiniot <louis99679@gmail.com>
2021-03-20 02:01:10 +0100

    agigunte loginGuard per route non raggiungibili

--- progetto/src/app/app-routing.module.ts
+++ progetto/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginGuardService } from './services/loginGuard/login-guard.service';
 
 const routes: Routes = [
   {path:'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)},
   {path:'login', loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule)},
-  {path:'profile', loadChildren: () => import('./components/profilo/profilo.module').then(m => m.ProfiloModule)},
-  {path:'tornei', loadChildren: () => import('./components/tornei/tornei.module').then(m => m.TorneiModule)},
-  {path:'miei-tornei', loadChildren: () => import('./components/miei-tornei/miei-tornei.module').then(m => m.MieiTorneiModule)},
+  {path:'profilo', loadChildren: () => import('./components/profilo/profilo.module').then(m => m.ProfiloModule),canActivate: [LoginGuardService]},
+  {path:'tornei', loadChildren: () => import('./components/tornei/tornei.module').then(m => m.TorneiModule),canActivate: [LoginGuardService]},
+  {path:'miei-tornei', loadChildren: () => import('./components/miei-tornei/miei-tornei.module').then(m => m.MieiTorneiModule),canActivate: [LoginGuardService]},
 ];
 
 @NgModule({
