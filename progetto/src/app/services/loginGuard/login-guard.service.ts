338d65b65008b727c1a685b7751154912f79fe5a
Louis-Guiniot <louis99679@gmail.com>
2021-03-20 01:55:35 +0100

    login guard filtro per pagine non raggiungibili prima della login

---
+++ progetto/src/app/services/loginGuard/login-guard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuardService implements CanActivate{
+
+  constructor(private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    if (sessionStorage.getItem('username') != null) {
+      return true;
+    } else {
+      window.alert("effettua il login per accedere a questa pagina");
+      this.router.navigateByUrl('/login');
+      return false;
+    }
+  }
+}
