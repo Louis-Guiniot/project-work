5a7ff6aac356de9058614ae5a937545a67489e98
Louis-Guiniot <louis99679@gmail.com>
2021-03-20 01:59:19 +0100

    sistemata schermata profilo, update funzionante / da fare delete ????

--- progetto/src/app/components/profilo/main/profilo.component.ts
+++ progetto/src/app/components/profilo/main/profilo.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Utente } from 'src/app/core/model/Utente.interface';
+import { selectUtente } from 'src/app/redux/utente';
+import { UtenteService } from 'src/app/services/utente/utente.service';
 
 @Component({
   selector: 'app-profilo',
@@ -7,9 +14,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfiloComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store, private router: Router, private fb: FormBuilder, private utenteService: UtenteService) { 
+    console.log(this.utenteService.elencoUtenti())
+  }
+
+  idutente = Number(sessionStorage.getItem('id'))
+
+  editaUtenteForm: FormGroup
 
   ngOnInit(): void {
+
+    console.log("utente: ",this.idutente)
+
+    this.editaUtenteForm = this.fb.group({
+      nome: ['', Validators.required],
+      cognome: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      email: ['', Validators.required],
+      genere: ['', Validators.required],
+      datanascita: ['', Validators.required],
+      
+    })
+
+  }
+
+  get utenti():Observable<Utente[]>{
+    return this.store.pipe(select(selectUtente))
+  }
+
+  edita(){
+    this.utenteService.aggiornaUtente(
+      this.idutente,
+      this.editaUtenteForm.value.nome,
+      this.editaUtenteForm.value.cognome,
+      this.editaUtenteForm.value.username,
+      this.editaUtenteForm.value.email,
+      this.editaUtenteForm.value.password,
+      this.editaUtenteForm.value.datanascita
+
+    )
+
+    window.location.reload()
   }
 
 }
