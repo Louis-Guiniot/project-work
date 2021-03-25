62b606c45faa77ddf03aa7c4451bcd7e7b4ca800
Louis-Guiniot <61980982+Louis-Guiniot@users.noreply.github.com>
2021-03-25 10:58:10 +0100

    fix module pagine registrazione e login

--- progetto/src/app/components/registrazione/registrazione.module.ts
+++ progetto/src/app/components/registrazione/registrazione.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginRoutingModule } from './login-routing.module';
+import { RegistrazioneRoutingModule } from './registrazione-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 
@@ -10,7 +10,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     SharedModule,
-    LoginRoutingModule
+    RegistrazioneRoutingModule
   ]
 })
-export class LoginModule { }
+export class RegistrazioneModule { }
