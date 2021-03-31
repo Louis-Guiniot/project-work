80e886a2965f8e952866071c283aca1ba73f542c
Louis-Guiniot <61980982+Louis-Guiniot@users.noreply.github.com>
2021-03-31 18:29:16 +0200

    aggionrato index redux

--- progetto/src/app/redux/index.ts
+++ progetto/src/app/redux/index.ts
@@ -1,5 +1,6 @@
 import { routerReducer, RouterReducerState } from "@ngrx/router-store";
 import { ActionReducerMap } from "@ngrx/store";
+import { classificaReducer, ClassificaState } from "./classifica/classifica.reducers";
 import { iscrizioneReducer, IscrizioneState } from "./iscrizioni/iscrizioni.reducers";
 import { torneoReducer, TorneoState } from "./miei-tornei/miei.tornei.reducers";
 import { utenteReducer, UtenteState } from "./utente/utente.reducers";
@@ -8,12 +9,14 @@ export interface AppState{
     router: RouterReducerState<any>;
     torneoState: TorneoState,
     utenteState: UtenteState,
-    iscrizioneState: IscrizioneState
+    iscrizioneState: IscrizioneState,
+    classificaState: ClassificaState
 }
 
 export const reducers: ActionReducerMap<AppState> = {
     router: routerReducer,
     torneoState: torneoReducer,
     utenteState: utenteReducer,
-    iscrizioneState: iscrizioneReducer
+    iscrizioneState: iscrizioneReducer,
+    classificaState: classificaReducer
 }
