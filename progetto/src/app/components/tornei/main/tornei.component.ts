db4ae3498377d372562eb7afaa8509ff411c0ea0
Louis-Guiniot <61980982+Louis-Guiniot@users.noreply.github.com>
2021-03-31 18:32:21 +0200

    fixata grafica + prima vista ultima classifica . da sistemare in un modal apposito che compare al click del btn iscriviti

--- progetto/src/app/components/tornei/main/tornei.component.ts
+++ progetto/src/app/components/tornei/main/tornei.component.ts
@@ -4,11 +4,14 @@ import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { Classifica } from 'src/app/core/model/Classifica';
 import { HttpCommunicationsService } from 'src/app/core/model/http/http-communications.service';
 import { Torneo } from 'src/app/core/model/Torneo.interface';
 import { Utente } from 'src/app/core/model/Utente.interface';
+import { selectClassifica } from 'src/app/redux/classifica';
 import { selectTorneo } from 'src/app/redux/miei-tornei';
 import { getCurrentUtente, selectUtente } from 'src/app/redux/utente';
+import { ClassificaService } from 'src/app/services/classifica/classifica.service';
 import { IscrizioniService } from 'src/app/services/iscrizioni/iscrizioni.service';
 import { MieiTorneiService } from 'src/app/services/miei-tornei/miei-tornei.service';
 import { UtenteService } from 'src/app/services/utente/utente.service';
@@ -20,13 +23,21 @@ import { UtenteService } from 'src/app/services/utente/utente.service';
 })
 export class TorneiComponent implements OnInit {
 
-  constructor(private fb:FormBuilder,private store: Store, private router: Router,
-    private mieiTorneiService: MieiTorneiService,private iscrizioniService: IscrizioniService,private utentiService: UtenteService,
-    private modalService: NgbModal, private http: HttpCommunicationsService) { 
-    // console.log(this.mieiTorneiService.elencoTorneiPerCreatore(sessionStorage.getItem('id')));
-    // console.log(this.mieiTorneiService.elencoTornei());
-    // console.log(this.utentiService.elencoUtenti());
-    console.log(this.mieiTorneiService.elencoTorneiExcept(this.idCreatore))
+  constructor(
+    private fb:FormBuilder,
+    private store: Store, 
+    private router: Router,
+    private mieiTorneiService: MieiTorneiService,
+    private iscrizioniService: IscrizioniService,
+    private utentiService: UtenteService,
+    private classificaService: ClassificaService,
+    private modalService: NgbModal, 
+    private http: HttpCommunicationsService) { 
+
+    this.utentiService.elencoUtenti()
+    this.mieiTorneiService.elencoTornei()
+    this.classificaService.classifica()
+    
 
   }
   
@@ -45,13 +56,8 @@ export class TorneiComponent implements OnInit {
     this.idTorneoDaVedere = idtorneo
   }
 
-  filtroGioco:FormGroup
-
   ngOnInit(): void {
 
-      this.filtroGioco = this.fb.group({
-        gioco : ['', Validators.required]
-      })
 
   }
 
@@ -63,51 +69,12 @@ export class TorneiComponent implements OnInit {
     return this.store.pipe(select(selectUtente))
   }
 
-  i = 0
-
-  // torneiFiltrati(){
-  //   console.log("f = " , this.filtroGioco.value.gioco)
-
-  //   this.store.pipe(select(selectTorneo)).subscribe((tornei) => {
-  //     tornei.forEach(torneo => {
-  //       if(torneo.gioco === this.filtroGioco.value.gioco){
-  //         this.torneiF.push(torneo)
-  //       }
-
-  //       this.torneiF.forEach(torneoFiltrato => {
-  //         if(torneoFiltrato.gioco !== this.filtroGioco.value.gioco){
-  //           this.torneiF.splice(this.i,1)
-  //           this.i = this.i + 1
-  //         }
-  //       })
-
-  //       this.i = 0
-
-
-  //     });
-  //   })
-
-  //   this.getTorneiP("ps5").subscribe(itemFound => {
-  //     this.arrayDedicato = itemFound
-  //     console.log("dedicat0 -- ", this.arrayDedicato)
-  //   })
-  // }
+  get classifica():Observable<Classifica[]>{
+    return this.store.pipe(select(selectClassifica))
+  }
 
   iscriviti(){
     this.iscrizioniService.iscriviUtente(this.idTorneoDaVedere, this.idCreatore)
-  }
-
-  // url: string
-  // arrayDedicato : Object = []
-
-  // getTorneiP(piattaforma: string): Observable<Torneo[]> {
-  //   this.url = 'torneo/elencotTorneiP'
-  //   return this.http.retrievePostCall<Torneo[]>(this.url, piattaforma).pipe()
-  // }
-
-  get torneiF(): Observable<Torneo[]> {
-    return this.http.retrievePostCall<Torneo[]>('torneo/elencoTorneiExcept', this.idCreatore)
-
   }
 
 }
