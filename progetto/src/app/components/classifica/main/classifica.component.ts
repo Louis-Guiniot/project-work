7731a4a51b54bbd727b43248f771b71843e716bc
Louis-Guiniot <61980982+Louis-Guiniot@users.noreply.github.com>
2021-04-07 14:19:45 +0200

    creato componente classifica

---
+++ progetto/src/app/components/classifica/main/classifica.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Classifica } from 'src/app/core/model/Classifica';
+import { Utente } from 'src/app/core/model/Utente.interface';
+import { selectClassifica } from 'src/app/redux/classifica';
+import { selectUtente } from 'src/app/redux/utente';
+import { ClassificaService } from 'src/app/services/classifica/classifica.service';
+import { MieiTorneiService } from 'src/app/services/miei-tornei/miei-tornei.service';
+import { UtenteService } from 'src/app/services/utente/utente.service';
+
+@Component({
+  selector: 'app-classifica',
+  templateUrl: './classifica.component.html',
+  styleUrls: ['./classifica.component.scss']
+})
+export class ClassificaComponent implements OnInit {
+
+  constructor(private classificaService: ClassificaService, private store: Store, private utenteService: UtenteService, private torneoService: MieiTorneiService) {
+
+    this.classificaService.classifica()
+    this.utenteService.elencoUtenti()
+    this.torneoService.elencoTornei()
+
+   }
+
+   utente = sessionStorage.getItem('username')
+
+   home = "/home"
+
+  ngOnInit(): void {
+
+    console.log("username", this.utente)
+
+  }
+
+
+  get classifica():Observable<Classifica[]>{
+    return this.store.pipe(select(selectClassifica))
+  }
+
+  get utenti():Observable<Utente[]>{
+    return this.store.pipe(select(selectUtente))
+  }
+
+}
