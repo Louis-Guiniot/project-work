9174848d42f88e54f7ed9bcbba05a4edafc2ef6e
Louis-Guiniot <61980982+Louis-Guiniot@users.noreply.github.com>
2021-04-07 14:13:01 +0200

    interfaccia classifica globale

---
+++ progetto/src/app/core/model/ClassificaGlobale.ts
@@ -0,0 +1,9 @@
+export interface ClassificaGlobale{
+    id?: number
+    idPlayer: number
+    torneiGiocati: number
+    punteggioTotale: number
+	mediaPunteggio: number
+	vittorie: number
+	mediaPodio: number
+}
