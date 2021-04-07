ee8e53c92b0a6669a2151bdd4fff79f6ccdde0d1
Louis-Guiniot <61980982+Louis-Guiniot@users.noreply.github.com>
2021-04-07 14:18:38 +0200

    metodo per mostrare classifica globale

--- progetto/src/app/components/home/main/home.component.ts
+++ progetto/src/app/components/home/main/home.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { ClassificaGlobale } from 'src/app/core/model/ClassificaGlobale';
+import { Utente } from 'src/app/core/model/Utente.interface';
+import { selectClassificaGlobale } from 'src/app/redux/classificaGlobale';
+import { selectUtente } from 'src/app/redux/utente';
+import { ClassificaGlobaleService } from 'src/app/services/classificaGlobale/classifica-globale.service';
+import { UtenteService } from 'src/app/services/utente/utente.service';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +15,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private classificaGlobaleService: ClassificaGlobaleService, 
+    private utenteService: UtenteService,
+    private store: Store) { 
 
-  images = [
-    {path:'./assets/carousel-img-1.png'},
-    {path:'./assets/carousel-img-2.jpg'},
-    {path:'./assets/carousel-img-3.jpg'},
-  ]
+    this.classificaGlobaleService.classificaGlobale()
+    this.utenteService.elencoUtenti();
 
+  }
 
   ngOnInit(): void {
   }
+
+  get classificaGlobale():Observable<ClassificaGlobale[]> {
+    return this.store.pipe(select(selectClassificaGlobale))
+  }
+
+  get utenti():Observable<Utente[]>{
+    return this.store.pipe(select(selectUtente))
+  }
+
 
 }
